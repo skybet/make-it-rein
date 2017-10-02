5a27d96e29fb4a649e13f5e4536fd79fb92f983f
Meurig Phillips <mph01@sbgml02474.sbg.skybettingandgaming.com>
2017-10-02 16:48:31 +0100

    admin pages

---
+++ views/adminHorseCreator.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Add Horse to Database</title>
+  </head>
+  <body>
+    <h1>Add Horse to Database</h1>
+    <form action="/action_page.php">
+      Horse Name: <input  type="text" name="HorseName" value="" id="x">
+
+      <button type="button" onclick=addHorseToArray()>+</button>
+      <ul id="list">
+      </ul>
+      <br>
+      <br>
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+  <script src="adminHorseJS.js"></script>
+</html>
---
+++ views/adminRaceGenerator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Race Generator</title>
+  </head>
+  <body>
+    <h1>Admin Race Generator</h1>
+
+    <p><b>Round</b></p>
+    <select>
+      <option value="1">1 - 04/09/17-10/09/17</option>
+      <option value="2">2 - 11/09/17-17/09/17</option>
+      <option value="3">3 - 18/09/17-24/09/17</option>
+      <option value="4">4 - 25/09/17-01/10/17</option>
+    </select>
+
+
+
+  </body>
+</html>
