c785b89869bfec2abaee586738bdf5a0fe2d7f13
Thomas Morris <tmo01@sbgml02476.sbg.skybettingandgaming.com>
2017-10-03 11:41:02 +0100

    db file and horse classes

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+function get_db()
+{
+    $host ='127.0.0.1';
+    $db = 'MakeItRein';
+    $userdb = 'user1';
+    $pass = 'mypass';
+
+   $dsn = "mysql:host=$host; dbname=$db";
+    $pdo = new PDO($dsn, $userdb, $pass);
+    return $pdo;
+}
+
+
+?>
