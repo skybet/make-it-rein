24a6c0aefda2e773f03aba2ae89b94e5df4a1583
jaketongue <jake.tongue@skybettingandgaming.com>
2017-10-04 14:38:47 +0100

    Race Generator Admin Page

--- views/adminRaceGenerator.php
+++ views/adminRaceGenerator.php
@@ -7,26 +7,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/fontAwesome/css/font-awesome.min.css"/>
-    <link rel="stylesheet" type="text/css" href="css/rein.css"/>
-    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/bootstrap/bootstrap.min.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/fontAwesome/css/font-awesome.min.css'?>"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/rein.css'?>"/>
+    <link rel="icon" href="<?php echo '../img/favicon.ico'?>" type="image/x-icon">
   </head>
   <body>
-    <?php include_once('includes/productHeader.inc.php'); ?>
-    <?php include_once('includes/navBar.inc.php'); ?>
-    <h1>Admin Race Generator</h1>
+    <?php include_once('../includes/productHeader.inc.php'); ?>
+    <?php include_once('../includes/navBar.inc.php'); ?>
 
-    <p><b>Round</b></p>
-    <select id="round">
-      <option value="1">1 - 04/09/17-10/09/17</option>
-      <option value="2">2 - 11/09/17-17/09/17</option>
-      <option value="3">3 - 18/09/17-24/09/17</option>
-      <option value="4">4 - 25/09/17-01/10/17</option>
-    </select>
-    
+    <div class="container">
+      <div>
+        <h1>Admin Race Generator</h1>
+        <p><b>Round</b></p>
+        <select id="round">
+          <option value="1">1 - 04/09/17-10/09/17</option>
+          <option value="2">2 - 11/09/17-17/09/17</option>
+          <option value="3">3 - 18/09/17-24/09/17</option>
+          <option value="4">4 - 25/09/17-01/10/17</option>
+        </select>
+      </div>
+    </div>
 
-
-<script src=""> </script>
+    <?php include_once('../includes/footer.inc.php'); ?>
   </body>
+    <script src="<?php echo '../js/jquery-3.2.1.min.js'?>"> </script>
+    <script src="<?php echo '../js/indexLogic.js'?>"> </script>
 </html>
