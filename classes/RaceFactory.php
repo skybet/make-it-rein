ee93fed536db7bfc56342577ce0bd9fdc6b928b7
Jessica Lettall <jle01@sbgml02463.sbg.skybettingandgaming.com>
2017-10-04 11:44:11 +0100

    Race class and race factory

---
+++ classes/RaceFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+class RaceFactory {
+  private $db;
+
+
+  public function __construct(PDO $db){
+    $this->db = $db;
+
+  }
+
+  public function byRoundId($roundId){
+    $s = $this->db->prepare("
+          select RoundId,
+              RaceId
+          from Race
+          where RoundId = :roundId
+    ");
+    $s->execute(['roundId' => $roundId]);
+    if (!$s){
+        return false;
+    }
+
+    $raceArray = [];
+
+    foreach ($s as $row) {
+      $race = new Race();
+      $race->id = $row['RaceId'];
+      $race->roundId = $row['RoundId'];
+
+      array_push($raceArray, $race);
+    }
+
+
+      return $raceArray;
+  }
+
+}
+
+?>
