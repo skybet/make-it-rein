3fe66a2e1b38aec9649a00c4d0cf3a1662492a9e
Meurig Phillips <mph01@sbgml02474.sbg.skybettingandgaming.com>
2017-10-04 12:17:46 +0100

    Started results generator

---
+++ logic/addResults.php
@@ -0,0 +1,17 @@
+<?php
+
+$r1p1 = $_POST['race1pos1'];
+$r1p2 = $_POST['race1pos2'];
+$r1p3 = $_POST['race1pos3'];
+$r2p1 = $_POST['race2pos1'];
+$r2p2 = $_POST['race2pos2'];
+$r2p3 = $_POST['race2pos3'];
+$r3p1 = $_POST['race3pos1'];
+$r3p2 = $_POST['race3pos2'];
+$r3p3 = $_POST['race3pos3'];
+
+echo $r1p2;
+echo $r3p3;
+
+
+ ?>
