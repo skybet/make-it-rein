4423be471ea2839be21930cd619b3d75aa2cbcf0
Mandy Thornton <mandythornton@hotmail.co.uk>
2017-10-05 11:23:07 +0100

    current t and c

---
+++ views/termsAndConditions.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Race Generator</title>
+    <title>Make it Rein</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/bootstrap/bootstrap.min.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/fontAwesome/css/font-awesome.min.css'?>"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo '../css/rein.css'?>"/>
+    <link rel="icon" href="<?php echo '../img/favicon.ico'?>" type="image/x-icon">
+  </head>
+  <body>
+    <?php include_once('../includes/productHeader.inc.php'); ?>
+    <?php include_once('../includes/navBar.inc.php'); ?>
+
+    <div class="container">
+      <div>
+            <!-- Terms and conditions  -->
+            <h2 class ="txt-ctr">Terms and Conditions</h2>
+
+
+            <P><h4>Players</h4></P>
+
+            <ol>
+                <li>This Competition is open to persons aged 18 years or over resident in the UK, Isle of Man, Channel Islands and Republic of Ireland. Proof of age and identity may be required. Entry to the Competition is free.
+                <li>Employees of Hestview Limited and Infinity Works Consulting directly involved in the operation of the Competition are not eligible to win prizes.</li>
+                <li>In order to enter the Competition players are required to enter their email address upon making their selections. Only one entry per week is allowed per person. Multiple entries will be disqualified. If the Promoter has reasonable grounds to believe that multiple entries have been made by the same person, all entries by such player and/or email addresses will be disqualified from participating in the Competition or receiving any prizes. Players are required to enter using their own email address and any player found to be entering using the email address of any other person will be disqualified.</li>
+                <li>Players will be deemed to have accepted these terms and conditions and agreed to be bound by them when entering this Competition. These terms and conditions will apply for the duration of the Competition.</li>
+                <li>In addition to the use specified above, the Promoter (Hestview Limited) will use each winner’s, and each entrant’s email address for the purpose of administering this Competition. By entering the Competition, you agree that Hestview Limited, Sky Betting and Gaming and their group companies may use the details you give us to contact you about each of our products and services that may be of interest to you. You can change your direct marketing preferences by contacting us on 0330 024 4777 or support@skybet.com. You can also click on the "unsubscribe" link in direct marketing emails from us.</li>
+            </ol>
+
+            <P><h4>The Competition</h4></P>
+            <ol>
+                <li>To take part in the Make it Rein Competition, a player must select the 1st, 2nd and 3rd placed horses in all three Make it Rein races.</li>
+                <li>Make it Rein selections must be made before the official scheduled off-time of the first Make it Rein race.
+                <li>The Competition closes before the start time of the first race in the Competition. Once the weekly competition has closed, entrants cannot change their weekly predictions. Once an entry has been submitted, it cannot be changed.</li>
+                <li>To win the Make it Rein Competition Jackpot Prize of £100,000 (unless otherwise specified), a player must correctly select the 1st, 2nd and 3rd placed horses in all three of the Make it Rein races. If the jackpot is won by multiple entrants, the Competition prize will be split equally amongst those players. The £100,000 prize (unless otherwise specified) can only be won if all three Make it Rein races are run (no abandoned races).
+                <li>If any Make it Rein race is postponed, abandoned or not completed the race will be considered void and the Competition prize will roll over to the following week.
+                <li>There is no cash alternative to the non-cash prizes and unless agreed otherwise in writing the prizes are non-refundable and non-transferable.
+                <li>The Promoter reserves the right to substitute the prizes (or any part of them) for a prize of equivalent or greater monetary value if this is necessary for reasons beyond its control.
+                <li>The Promoter will not be liable for prizes that do not reach the winners for reasons beyond its reasonable control.
+            </ol>
+
+            <P><h4>Winners</h4></P>
+            <ol>
+                <li>Winners will be notified by email, using the address provided upon entering the competition. It is each player’s sole responsibility to ensure that their email address is correct. Winners will be required to verify (with evidence satisfactory to the Promoter) their age, identity, residence and other requested verification information to the satisfaction of the Promoter before they are eligible to receive a prize. Any public announcement (including on Sky properties (TV, online etc.)) will be pending verification unless otherwise stated and should not be considered final confirmation.
+                <li>If any player is not able, for whatever reason, to accept a prize, verify their registration information to the satisfaction of the Promoter, has been found to have breached these terms and conditions by the Promoter or cannot be contacted within five (5) Business Days (a day other than a Saturday, Sunday or public holiday in England when banks in London are open for business), the Promoter reserves the right to disqualify that player (and their eligibility to receive any prizes under the Competition) and award the prize to another participant, if applicable.
+                <li>Entry into the Competition constitutes permission for Hestview Ltd and/or their associated, affiliated, partnered and/or subsidiary companies to use the names and photographs of participants for publicity purposes at its discretion. All winners may be required to participate in such publicity without further compensation as the Promoter reasonably requires.
+                <li>Allow 45 days for prizes for payment and delivery of prizes.</li>
+            </ol>
+
+            <P><h4>Submission of Entries</h4></P>
+            <ol>
+                <li>The Promoter does not accept responsibility for network, computer hardware or software failures of any kind, which may restrict or delay the sending or receipt of your entry. Under no circumstances can a user submit their entry via e-mail.
+            </ol>
+
+            <P><h4>Limitation of Liability</h4></P>
+            <ol>
+                <li>Except in the case of death or personal injury arising from its negligence or in respect of fraud and so far as is permitted by law, the Promoter and its associated and partner companies and agents exclude responsibility and all liabilities arising from:</li>
+                    <ol type="a">
+                    <li>any postponement, cancellation, delay or changes to the prizes, promotions, Competition or relevant racing fixtures beyond the Promoter’s reasonable control;<br>
+                    <li>any technical failures or unavailability of the Make it Rein Website, Mobile Apps or other related platforms;<br>
+                    <li>obvious errors or omissions relating to the awarding of prizes; or<br>
+                    <li>for any act or default of any third party supplier including without limitation errors relating to fixtures and scoring data.<br>
+                    <li>The terms and conditions of any third party supplier will apply to the prize where applicable.</ol>
+                <li>If any provision or part-provision of these terms and conditions are or becomes invalid, illegal or unenforceable, it shall be deemed modified to the minimum extent necessary to make it valid, legal and enforceable. If such modification is not possible, the relevant provision or part-provision shall be deemed deleted. Any modification to or deletion of a provision or part-provision under this clause shall not affect the validity and enforceability of the rest of these terms and conditions.
+            </ol>
+
+            <P><h4>Promoter</h4></P>
+            <ol>
+                <li>The Promoter's decision is final and legally binding on all entrants in relation to all aspects of the Competition including (without limitation) allocation of the prizes and no correspondence will be entered into. Entries that do not comply in full with the Competition’s rules or terms and conditions will be disqualified and will not be eligible to receive any prizes.
+                <li>The promotion is governed by the laws of England and Wales and the parties submit to the exclusive jurisdiction of the English courts.
+                <li>The Promoter is Hestview Limited, a company incorporated in England with company number 1100741, whose principal place of business is at 2 Wellington Place, Leeds, West Yorkshire, LS1 4AP.
+            </ol><br>
+
+These Terms and Conditions were last updated on 4 October 2017.<br><br>
+
+      </div>
+    </div>
+
+    <?php include_once('../includes/footer.inc.php'); ?>
+  </body>
+    <script src="<?php echo '../js/jquery-3.2.1.min.js'?>"> </script>
+    <script src="<?php echo '../js/indexLogic.js'?>"> </script>
+</html>
