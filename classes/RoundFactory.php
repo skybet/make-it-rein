df860c4de5b5fe64f2039e64111bd61e4c55d939
Meurig Phillips <mph01@sbgml02474.sbg.skybettingandgaming.com>
2017-10-05 13:55:39 +0100

    Started select round for results generator

--- classes/RoundFactory.php
+++ classes/RoundFactory.php
@@ -3,11 +3,20 @@
 class RoundFactory {
 
     private $db;
-    
+
     public function __construct(PDO $db){
         $this->db = $db;
     }
 
+    public function getAllRounds(){
+      $rounds = [];
+
+      $q = $this->db->query("SELECT * FROM Round");
+      foreach ($q as $index => $round) {
+        $rounds[$index] = $round
+      }
+      return rounds;
+    }
 
 }
 
